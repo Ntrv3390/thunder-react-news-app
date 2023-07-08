e88eca7ee8f0cdbc3486a7b8eaa562c378eaf4ae
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2023-07-08 18:37:29 +0530

    Dynamic news via news api key

--- src/Modules/Navbar.js
+++ src/Modules/Navbar.js
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 export class Navbar extends Component {
   render() {
     const myStyle = {
-      
       position: "fixed",
       width: "100%",
       top: "0",
-      zIndex: "10"
-    }
+      zIndex: "10",
+    };
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={myStyle}>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        style={myStyle}
+      >
         <a className="navbar-brand mx-3" href="/">
           Thunder - News App
         </a>
@@ -22,8 +24,32 @@ export class Navbar extends Component {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" target="_blanck" href="https://ntrv3390.github.io/react-text-utils/">
+              <a
+                className="nav-link"
+                target="_blanck"
+                href="https://ntrv3390.github.io/react-text-utils/"
+              >
                 TextUtils
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://github.com/Ntrv3390"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://linkedin.com/in/mohammed-puthawala"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIN
               </a>
             </li>
           </ul>
