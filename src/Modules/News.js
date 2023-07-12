8b47da14d38a2478bead5bfda0c19634c96f213a
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2023-07-12 11:33:04 +0530

    Added previous and next buttons

--- src/Modules/News.js
+++ src/Modules/News.js
@@ -2,18 +2,67 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {  
+  constructor(){
+    super();
+    this.state = {
+        articles: [],
+        page: 1,
+        pageSize: 12
+    }
+  }
+  async componentDidMount()
+  {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=64b16658e2ea4245be339d32e17be79e&page=${this.state.page}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    });
+  }
+  handlePrevNews = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=64b16658e2ea4245be339d32e17be79e&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    });
+    window.scrollTo(0, 0);
+  }
+  handleNextNews = async () => {
+    
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=64b16658e2ea4245be339d32e17be79e&page=${this.state.page + 1}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+    });
+    window.scrollTo(0, 0);
+  }
   render() {
     let {title} = this.props;
     const containerStyle = {
         margin:"80px"
     }
     return (
+      <>
       <div className="container mt-5 mb-5" >
         <h2 className="text-center mb-5" style={containerStyle}>{title} </h2>
         <div className="row">
-            <Newsitem/>
+        {this.state.articles.map((element)=>{
+           return <div className="col-md-3 mt-4 col-sm-6 col-lg-3" key={element.url}>
+            <Newsitem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            </div>
+          })}
         </div>
       </div>
+      <div className="container d-flex justify-content-around">
+      <button disabled={this.state.page <= 1} onClick={this.handlePrevNews} type="button" className="btn btn-primary">&larr; Previous</button>
+      <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.state.pageSize)} type="button" onClick={this.handleNextNews} className="btn btn-primary">Next &rarr;</button>
+      </div>
+    </>
     )
   }
 }
--- src/Modules/Newsitem.js
+++ src/Modules/Newsitem.js
@@ -1,45 +1,28 @@
 import React, { Component } from "react";
 
 export class Newsitem extends Component {
-  articles = [];
-  constructor(){
-    super();
-    this.state = {
-        articles: this.articles,
-    }
-  }
-  async componentDidMount()
-  {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=64b16658e2ea4245be339d32e17be79e";
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({articles: parsedData.articles});
-  }
+
 
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <>
-      {this.state.articles.map((element)=>{
-        return <div className="col-md-3 mt-4" key={element.url}>
         <div className="card" style={{ width: "18rem" }}>
-        <a href={element.url?element.url:"https://www.bbc.co.uk/news/av/world-us-canada-66071216"} target="_blanck">
+        <a href={newsUrl?newsUrl:"https://www.bbc.co.uk/news/av/world-us-canada-66071216"} target="_blanck">
           <img
-            src={element.urlToImage?element.urlToImage:"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}
+            src={imageUrl?imageUrl:"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}
             className="card-img-top"
             alt="abc"
           />
           </a>
           <div className="card-body">
-            <h5 className="card-title">{element.title?element.title.slice(0,45):""}...</h5>
-            <p className="card-text">{element.description?element.description.slice(0,90):""}...</p>
-            <a href={element.url?element.url:"https://www.bbc.co.uk/news/av/world-us-canada-66071216"} target="_blanck" className="btn btn-outline-primary">
-              Read More
+            <h5 className="card-title">{title?title.slice(0,45):""}...</h5>
+            <p className="card-text">{description?description.slice(0,90):""}...</p>
+            <a href={newsUrl?newsUrl:"https://www.bbc.co.uk/news/av/world-us-canada-66071216"} target="_blanck" className="btn btn-outline-primary">
+              Read More &rarr;
             </a>
           </div>
         </div>
-      </div>
-      })}
-        
       </>
     );
   }
